0feb2aa137958203bbe42debabf9a2ba7766c2b6
sd19surf <sd19surf@gmail.com>
2021-11-17 17:07:08 -0500

    added working end point

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM python:latest
 COPY . .
+RUN pip install -r requirements.txt
 EXPOSE 8080
-ENTRYPOINT ["gunicorn", "-w", "3", "--bind=0.0.0.0:8080"]
+ENTRYPOINT ["gunicorn", "-w", "3", "--bind=0.0.0.0:8080", "app:app"]
--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 from flask import Flask
 import gunicorn
 
-"""Test another comment another comment"""
+app = Flask(__name__)
+
+@app.route("/")
+def entrypoints():
+    entrypoint_dict = {}
+    entrypoint_dict["entrypoint_url"] = "/"
+    return entrypoint_dict
